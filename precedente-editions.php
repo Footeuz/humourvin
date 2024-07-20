15288a482df78d3d5ab9ee93e2834fac1d9eabb4
Footeuz <footeuz@gmail.com>
2024-07-20 17:01:44 +0200

    Zoom system

--- precedente-editions.php
+++ precedente-editions.php
@@ -1,7 +1,29 @@
 
 <?php
-    $page=4;
-    include('./_header.php');
+$page=4;
+include('./_header.php');
+
+$pictures[1]['path'] = './assets/Affiche-BourgesHumourVin-2023.jpg';
+$pictures[1]['caption'] = 'Affiche festival 2023';
+$pictures[1]['year'] = '2023';
+$pictures[2]['path'] = './assets/Affiche-BourgesHumourVin-2022.jpg';
+$pictures[2]['caption'] = 'Affiche festival 2022';
+$pictures[2]['year'] = '2022';
+$pictures[3]['path'] = './assets/Affiche-BourgesHumourVin-2021.jpg';
+$pictures[3]['caption'] = 'Affiche festival 2021';
+$pictures[3]['year'] = '2021';
+$pictures[4]['path'] = './assets/Affiche-BourgesHumourVin-2020.jpg';
+$pictures[4]['caption'] = 'Affiche festival 2020';
+$pictures[4]['year'] = '2020';
+$pictures[5]['path'] = './assets/Affiche-BourgesHumourVin-2019.jpg';
+$pictures[5]['caption'] = 'Affiche festival 2019';
+$pictures[5]['year'] = '2019';
+$pictures[6]['path'] = './assets/Affiche-BourgesHumourVin-2018.jpg';
+$pictures[6]['caption'] = 'Affiche festival 2018';
+$pictures[6]['year'] = '2018';
+$pictures[7]['path'] = './assets/Affiche-BourgesHumourVin-2017.jpg';
+$pictures[7]['caption'] = 'Affiche festival 2017';
+$pictures[7]['year'] = '2017';
 ?>
 
 <section id="four" class="header-content">
@@ -15,112 +37,32 @@
     </div>
     <div class="container-fluid">
         <div class="row no-gutter">
-            <div class="col-lg-4 col-sm-4 text-center">
-                <a class="gallery-box" onclick="openModal();currentSlide(1)">
-                    <img src="./assets/Affiche-BourgesHumourVin-2023.jpg" height="250" alt="Affiche festival 2023" />
-                    <div class="gallery-box-caption">
-                        <div class="gallery-box-content">
-                            <div>
-                                <i class="icon-lg ion-ios-search"></i>
+            <?php foreach($pictures as $cpt => $picture) { ?>
+                <div class="col-lg-4 col-sm-4 text-center">
+                    <a class="gallery-box" onclick="openModal();currentSlide(<?= $cpt ?>)">
+                        <img src="<?= $picture['path'] ?>" height="250" alt="<?= $picture['caption'] ?>" />
+                        <div class="gallery-box-caption">
+                            <div class="gallery-box-content">
+                                <div>
+                                    <i class="icon-lg ion-ios-search"></i>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </a>
-                <h2 class="mt-3">2023</h2>
-            </div>
-
-            <div class="col-lg-4 col-sm-4 text-center">
-                <a class="gallery-box" onclick="openModal();currentSlide(2)">
-                    <img src="./assets/Affiche-BourgesHumourVin-2022.jpg" height="250" alt="Affiche festival 2022" />
-                    <div class="gallery-box-caption">
-                        <div class="gallery-box-content">
-                            <div>
-                                <i class="icon-lg ion-ios-search"></i>
-                            </div>
-                        </div>
-                    </div>
-                </a>
-                <h2 class="mt-3">2022</h2>
-            </div>
-            <div class="col-lg-4 col-sm-4 text-center">
-                <a class="gallery-box" onclick="openModal();currentSlide(3)">
-                    <img src="./assets/Affiche-BourgesHumourVin-2021.jpg" height="250" alt="Affiche festival 2021" />
-                    <div class="gallery-box-caption">
-                        <div class="gallery-box-content">
-                            <div>
-                                <i class="icon-lg ion-ios-search"></i>
-                            </div>
-                        </div>
-                    </div>
-                </a>
-                <h2 class="mt-3">2021</h2>
-            </div>
-            <div class="col-lg-4 col-sm-4 text-center">
-                <a class="gallery-box" onclick="openModal();currentSlide(4)">
-                    <img src="./assets/Affiche-BourgesHumourVin-2020.jpg" height="250" alt="Affiche festival 2020" />
-                    <div class="gallery-box-caption">
-                        <div class="gallery-box-content">
-                            <div>
-                                <i class="icon-lg ion-ios-search"></i>
-                            </div>
-                        </div>
-                    </div>
-                </a>
-                <h2 class="mt-3">2020</h2>
-                <p>Edition annulée à cause du contexte sanitaire</p>
-                <br/><br/><br/>&nbsp;
-            </div>
-            <div class="col-lg-4 col-sm-4 text-center">
-                <a class="gallery-box" onclick="openModal();currentSlide(5)">
-                    <img src="./assets/Affiche-BourgesHumourVin-2019.jpg" height="250" alt="Affiche festival 2019" />
-                    <div class="gallery-box-caption">
-                        <div class="gallery-box-content">
-                            <div>
-                                <i class="icon-lg ion-ios-search"></i>
-                            </div>
-                        </div>
-                    </div>
-                </a>
-                <h2 class="mt-3">2019</h2>
-            </div>
-            <div class="col-lg-4 col-sm-4 text-center">
-                <a class="gallery-box" onclick="openModal();currentSlide(6)">
-                    <img src="./assets/Affiche-BourgesHumourVin-2018.jpg" height="250" alt="Affiche festival 2018" />
-                    <div class="gallery-box-caption">
-                        <div class="gallery-box-content">
-                            <div>
-                                <i class="icon-lg ion-ios-search"></i>
-                            </div>
-                        </div>
-                    </div>
-                </a>
-                <h2 class="mt-3">2018</h2>
-            </div>
-            <div class="col-lg-4 col-sm-4 text-center">
-                <a class="gallery-box" onclick="openModal();currentSlide(7)">
-                    <img src="./assets/Affiche-BourgesHumourVin-2017.jpg" height="250" alt="Affiche festival 2017" />
-                    <div class="gallery-box-caption">
-                        <div class="gallery-box-content">
-                            <div>
-                                <i class="icon-lg ion-ios-search"></i>
-                            </div>
-                        </div>
-                    </div>
-                </a>
-                <h2 class="mt-3">2017</h2>
-            </div>
+                    </a>
+                    <h2 class="mt-3"><?= $picture['year'] ?></h2>
+                    <?php if ($picture['year'] == 2020) { ?><p>Edition annulée à cause du contexte sanitaire</p><?php } ?>
+                </div>
+            <?php } ?>
         </div>
+
         <div id="myModal" class="modal">
             <span class="close cursor" onclick="closeModal()">&times;</span>
             <div class="modal-content">
-
-                <?php $cpt = 1; $nb_pictures = date('Y')-2017; ?>
-                <?php for ($year = date('Y')-1; $year >= 2017; $year--) { ?>
+                <?php foreach($pictures as $cpt => $picture) { ?>
                     <div class="mySlides text-center bg-secondary">
-                        <div class="numbertext fw-bold text-white"><?= $cpt ?> / <?= $nb_pictures ?><br/><br/><?= $year ?></div>
-                        <img src="./assets/Affiche-BourgesHumourVin-<?= $year ?>.jpg" height="600" class="img-responsive" alt="" />
+                        <div class="numbertext fw-bold text-white"><?= $cpt ?> / <?= count($pictures) ?><br/><br/><?= $picture['year'] ?></div>
+                        <img src="<?= $picture['path'] ?>" height="600" class="img-responsive" alt="<?= $picture['caption'] ?>" />
                     </div>
-                    <?php $cpt++; ?>
                 <?php } ?>
 
                 <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
